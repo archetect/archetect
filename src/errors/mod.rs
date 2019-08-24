8f939f72448b498c20870dcb97ca1f002efe8328
Jimmie Fulton <jimmie.fulton@gmail.com>
2019-08-23 22:59:04 -0700

    Refactor rendering.

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,7 +1,15 @@
 use std::path::PathBuf;
 
+#[derive(Debug)]
 pub enum RenderError {
-    PathRenderError{ source: PathBuf, message: String },
-    FileRenderError{ source: PathBuf, message: String },
-    IOError { message: String },
+    PathRenderError{ source: PathBuf, error: crate::template_engine::Error, message: String },
+    FileRenderError{ source: PathBuf, error: crate::template_engine::Error, message: String },
+    IOError { error: std::io::Error, message: String },
+}
+
+impl From<std::io::Error> for RenderError {
+    fn from(error: std::io::Error) -> Self {
+        let message = error.to_string();
+        RenderError::IOError { error, message }
+    }
 }
