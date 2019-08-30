4794eb99b62713f0cafb65b0d1b79d8b864cab31
Jimmie Fulton <jimmie.fulton@gmail.com>
2019-08-30 16:06:57 -0700

    Get tests using the root archetypes working again.

--- archetect-lib/tests/archetype_render_test.rs
+++ archetect-lib/tests/archetype_render_test.rs
@@ -1,15 +1,17 @@
 use archetect::system::layout::LayoutType;
 use archetect::ArchetectError;
+use std::fs;
+use archetect::archetype::ArchetypeError::ArchetypeInvalid;
 
 #[test]
 fn render_archetypes() -> Result<(), ArchetectError> {
-//    for entry in fs::read_dir("./archetypes")? {
-//        let entry = entry?;
-//        let path = entry.path();
-//        if path.is_dir() {
-//            println!("{}", entry.path().display());
-//        }
-//    }
+    for entry in fs::read_dir("../archetypes").map_err(|e|ArchetectError::GenericError("Whoops!".to_owned())).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_dir() {
+            println!("{}", entry.path().display());
+        }
+    }
 
     let archetect = archetect::Archetect::builder().with_layout_type(LayoutType::Temp)?.build()?;
     println!("{}", archetect.layout().cache_dir().display());
