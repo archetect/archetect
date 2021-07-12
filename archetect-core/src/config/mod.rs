903f90289e72eb851fe63fd00d4fb822676c91da
Jimmie Fulton <jimmie.fulton@gmail.com>
2021-07-11 21:41:37 -0700

    Rename catalog2 to catalog

--- archetect-core/src/config/mod.rs
+++ archetect-core/src/config/mod.rs
@@ -1,13 +1,11 @@
 mod answers;
 mod archetype;
 mod catalog;
-mod catalog2;
 mod rule;
 mod variable;
 
 pub use answers::{AnswerConfig, AnswerConfigError, AnswerInfo};
 pub use archetype::ArchetypeConfig;
-pub use catalog::{CatalogConfig, CatalogConfigEntry, CatalogConfigEntryType, CatalogConfigError};
-pub use catalog2::{Catalog, CatalogEntry, CatalogError, CATALOG_FILE_NAME};
+pub use catalog::{Catalog, CatalogEntry, CatalogError, CATALOG_FILE_NAME};
 pub use rule::{Pattern, RuleAction, RuleConfig};
 pub use variable::{VariableInfo, VariableInfoBuilder, VariableType};
