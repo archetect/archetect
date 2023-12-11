0f92523fe47147803a7297387d31f801613df656
Jimmie Fulton <jimmie.fulton@gmail.com>
2023-12-11 11:39:17 -0800

    Add render_args to default subcommand

--- archetect-cli/src/cli.rs
+++ archetect-cli/src/cli.rs
@@ -13,24 +13,27 @@ pub fn command() -> Command {
     command!()
         .name("archetect")
         .help_expected(true)
+        .args(render_args())
         .arg(
             Arg::new("verbosity")
                 .help("Increase verbosity level")
                 .short('v')
                 .long("verbose")
                 .action(ArgAction::Count)
-                .global(true)
+                .global(true),
         )
         .arg(
             Arg::new("config-file")
                 .help("Supply an additional configuration file.")
-                .long_help("Supply an additional configuration file to supplement or override \
-                user and/or default configuration.")
+                .long_help(
+                    "Supply an additional configuration file to supplement or override \
+                user and/or default configuration.",
+                )
                 .long("config-file")
                 .short('c')
                 .action(ArgAction::Set)
                 .global(true)
-                .value_name("config")
+                .value_name("config"),
         )
         .subcommand(
             Command::new("render")
@@ -42,7 +45,7 @@ pub fn command() -> Command {
                         .action(ArgAction::Set)
                         .required(true),
                 )
-                .args(render_args())
+                .args(render_args()),
         )
         .subcommand(
             Command::new("catalog")
@@ -53,7 +56,7 @@ pub fn command() -> Command {
                         .action(ArgAction::Set)
                         .required(true),
                 )
-                .args(render_args())
+                .args(render_args()),
         )
         .subcommand(
             Command::new("completions")
@@ -62,8 +65,8 @@ pub fn command() -> Command {
                 .arg(
                     Arg::new("generator")
                         .help("Shell Generator")
-                        .value_parser(value_parser!(Shell))
-                )
+                        .value_parser(value_parser!(Shell)),
+                ),
         )
         .subcommand(
             Command::new("system")
@@ -80,42 +83,45 @@ pub fn command() -> Command {
                                 .about("The location where http resources are cached.  Used for offline mode."),
                         )
                         .subcommand(
-                            Command::new("config")
-                                .about("The location where archetect config files are stored."),
+                            Command::new("config").about("The location where archetect config files are stored."),
                         )
-                        .subcommand(
-                            Command::new("answers").about("The location where answers are specified."),
-                        ),
+                        .subcommand(Command::new("answers").about("The location where answers are specified.")),
                 ),
         )
         .subcommand(
             Command::new("config")
                 .arg_required_else_help(true)
                 .about("Manage Archetect's configuration")
-                .subcommand(Command::new("merged")
-                    .about("Show Archetect's merged configuration after applying command line arguments, \
-                    environment variables, and configuration files."))
-                .subcommand(Command::new("defaults")
-                    .about("Show Archetect's default configuration, which may be used for re-creating \
-                    a configuration file."))
+                .subcommand(Command::new("merged").about(
+                    "Show Archetect's merged configuration after applying command line arguments, \
+                    environment variables, and configuration files.",
+                ))
+                .subcommand(Command::new("defaults").about(
+                    "Show Archetect's default configuration, which may be used for re-creating \
+                    a configuration file.",
+                )),
         )
         .subcommand(
             Command::new("cache")
                 .about("Manage/Select from Archetypes cached from Git Repositories")
-                .subcommand(
-                    Command::new("manage")
-                        .about("Manage Archetect's cache through a Catalog")
-                )
-                .subcommand(Command::new("clear")
-                    .about("Removes Archetect's entire Repository Cache"))
-                .subcommand(Command::new("pull")
-                    .about("Pull all Archetypes and Catalogs in Archetect's Catalog")
-                )
+                .subcommand(Command::new("manage").about("Manage Archetect's cache through a Catalog"))
+                .subcommand(Command::new("clear").about("Removes Archetect's entire Repository Cache"))
+                .subcommand(Command::new("pull").about("Pull all Archetypes and Catalogs in Archetect's Catalog")),
         )
 }
 
 fn render_args() -> Vec<Arg> {
     let mut args = vec![];
+    args.push(
+        Arg::new("answer")
+            .help("Supply a key=value pair as an answer to a variable question.")
+            .long_help(VALID_ANSWER_INPUTS)
+            .long("answer")
+            .short('a')
+            .action(ArgAction::Append)
+            .value_name("prompt key=value"),
+    );
+
     args.push(
         Arg::new("answer-file")
             .help("Supply an answers file in JSON, YAML, or Rhai format as answers to variable questions.")
@@ -127,16 +133,6 @@ fn render_args() -> Vec<Arg> {
             .short('A')
             .action(ArgAction::Append)
             .value_name("path"),
-    );
-
-    args.push(
-        Arg::new("answer")
-            .help("Supply a key=value pair as an answer to a variable question.")
-            .long_help(VALID_ANSWER_INPUTS)
-            .long("answer")
-            .short('a')
-            .action(ArgAction::Append)
-            .value_name("prompt key=value"),
     );
 
     args.push(
@@ -158,19 +154,19 @@ fn render_args() -> Vec<Arg> {
     );
 
     args.push(
-        Arg::new("destination")
-            .help("The directory to render the Archetype in to")
-            .default_value(".")
-            .action(ArgAction::Set),
-    );
-
-    args.push(
         Arg::new("switches")
             .help("Enable switches that may trigger functionality within Archetypes")
             .long("switch")
             .short('s')
             .action(ArgAction::Append)
-            .value_name("switch name")
+            .value_name("switch name"),
+    );
+
+    args.push(
+        Arg::new("destination")
+            .help("The directory to render the Archetype in to")
+            .default_value(".")
+            .action(ArgAction::Set),
     );
 
     args.push(
@@ -179,34 +175,31 @@ fn render_args() -> Vec<Arg> {
             .short('o')
             .long("offline")
             .env("ARCHETECT_OFFLINE")
-            .action(ArgAction::SetTrue)
+            .action(ArgAction::SetTrue),
     );
-        args.push(
+    args.push(
             Arg::new("headless")
                 .help("Expect all inputs to be resolved by answers, defaults, and optional values, never waiting on interactive user input.")
                 .long("headless")
                 .env("ARCHETECT_HEADLESS")
                 .action(ArgAction::SetTrue)
-                .global(true)
         );
-        args.push(
-            Arg::new("local")
-                .help("Use local development checkouts where available and configured")
-                .long("local")
-                .short('l')
-                .env("ARCHETECT_LOCAL")
-                .action(ArgAction::SetTrue)
-                .global(true)
-        );
-        args.push(
-            Arg::new("force-update")
-                .help("Force updates for all Catalogs and Archetypes when rendering")
-                .long("force-update")
-                .short('U')
-                .env("ARCHETECT_FORCE_UPDATE")
-                .action(ArgAction::SetTrue)
-                .global(true)
-        );
+    args.push(
+        Arg::new("local")
+            .help("Use local development checkouts where available and configured")
+            .long("local")
+            .short('l')
+            .env("ARCHETECT_LOCAL")
+            .action(ArgAction::SetTrue)
+    );
+    args.push(
+        Arg::new("force-update")
+            .help("Force updates for all Catalogs and Archetypes when rendering")
+            .long("force-update")
+            .short('U')
+            .env("ARCHETECT_FORCE_UPDATE")
+            .action(ArgAction::SetTrue)
+    );
     args
 }
 
