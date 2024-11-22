9e43ac0022185e93ddd75302ecb600b2112a2a86
Jimmie Fulton <jimmie.fulton@gmail.com>
2024-11-22 10:11:56 -0800

    Clean up build.rs for Windows Manifest

--- archetect-bin/build.rs
+++ archetect-bin/build.rs
@@ -8,17 +8,15 @@ fn configure_windows() {
     let Ok(target_os) = std::env::var("CARGO_CFG_TARGET_OS") else { return };
     let Ok(target_env) = std::env::var("CARGO_CFG_TARGET_ENV") else { return };
 
-    let Ok(mut manifest) = std::env::current_dir() else { return };
-    println!("Current Directory: {:?}", manifest);
-    manifest.push(MANIFEST);
-    let Some(manifest) = manifest.to_str() else { return };
-
-    println!("manifest: {}", manifest);
-
     if !(target_os == "windows" && target_env == "msvc") {
-        println!("Not Windows on MSVC; skipping");
         return;
     }
+
+    println!("Windows on MSVC - embedding Manifest");
+
+    let Ok(mut manifest) = std::env::current_dir() else { return };
+    manifest.push(MANIFEST);
+    let Some(manifest) = manifest.to_str() else { return };
 
     println!("cargo:rerun-if-changed={}", MANIFEST);
     // Embed the Windows application manifest file.
